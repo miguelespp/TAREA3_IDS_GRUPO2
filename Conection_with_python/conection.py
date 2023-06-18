01ad2b756438e881f7c06e8e9c25befece893412
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2023-06-17 22:42:22 -0500

    Conexión con MongoDB

---
+++ Conection_with_python/conection.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+MONGODB_URI = "mongodb+srv://Nico1234:Nico1234@cluster0.a8zlarc.mongodb.net/"
+
+client = MongoClient(MONGODB_URI)
+
+for db_name in client.list_database_names():
+    print(db_name)
